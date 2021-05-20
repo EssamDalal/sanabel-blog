ec3921d3fa7500fdc34928b0cdae895bd1e1d35e
Essam Dalal <essamdalal7@gmail.com>
2021-05-21 02:34:09 +0300

    add authentication

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,16 @@ use App\Models\Tag;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Route;
 
-
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 Route::get('/', [PageController::class ,'home'])->name('home');
 
 
@@ -25,3 +34,8 @@ Route::resource('categories', CategoryController::class);
 
 Route::resource('tags',TagController::class);
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
